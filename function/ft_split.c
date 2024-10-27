7b1050055eb5a4772df1446e5aadcf84d9593d9e
elmehdi elkabia <elmehdielkabia@gmail.com>
2024-10-27 14:54:43 +0100

    all is good

--- function/ft_split.c
+++ function/ft_split.c
@@ -3,16 +3,116 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eelkabia <eelkabia@student.42.fr>          +#+  +:+       +#+        */
+/*   By: eelkabia <eelkabia@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/25 12:14:50 by eelkabia          #+#    #+#             */
-/*   Updated: 2024/10/25 12:15:19 by eelkabia         ###   ########.fr       */
+/*   Created: 2024/10/26 15:50:00 by eelkabia          #+#    #+#             */
+/*   Updated: 2024/10/27 12:54:51 by eelkabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static	size_t	count_word(char	*str, char c)
+{
+	size_t	i;
+	size_t	count;
+
+	i = 0;
+	count = 0;
+	while (str[i] != '\0')
+	{
+		if ((i == 0 || str[i - 1] == c) && str[i] != c)
+			count++;
+		i++;
+	}
+	return (count);
+}
+
+static	void	ft_free(char **result, size_t word_index)
+{
+	while (word_index > 0)
+	{
+		free(result[word_index]);
+		word_index--;
+	}
+	free(result);
+}
+
+static	char	*word(const char *s, size_t n)
+{
+	size_t	i;
+	char	*dup ;
+
+	i = 0;
+	dup = (char *)malloc((n + 1) * sizeof(char));
+	if (!dup)
+		return (NULL);
+	while (i < n)
+	{
+		dup[i] = s[i];
+		i++;
+	}
+	dup[n] = '\0';
+	return (dup);
+}
+
+static	char	**split_dup(char **result, const char *s, char c)
+{
+	size_t	i;
+	size_t	start;
+	size_t	word_index;
+
+	i = 0;
+	word_index = 0;
+	start = 0;
+	while (s[i])
+	{
+		if (s[i] != c)
+		{
+			start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			result[word_index] = word(s + start, i - start);
+			if (!result[word_index])
+				ft_free(result, word_index);
+			word_index++;
+		}
+		i++;
+	}
+	result[word_index] = NULL;
+	return (result);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**result;
+	size_t	count;
+	size_t	i;
+
+	count = count_word((char *)s, c);
+	result = (char **)malloc(sizeof(char *) * (count + 1));
+	if (!result)
+		return (NULL);
+	result = split_dup(result, s, c);
+	return (result);
 }
+
+// int	main(void)
+// {
+// 	char	*str = "elmehdi elkabia, hello elmehdi elkabia";
+// 	char	c = ',';
+// 	char	**words = ft_split(str, c);
+// 	size_t	i = 0;
+
+// 	if (words)
+// 	{
+// 		while (words[i])
+// 		{
+// 			printf("%s\n", words[i]);
+// 			free(words[i]);
+// 			i++;
+// 		}
+// 		free(words);
+// 	}
+// 	return (0);
+// }
--- function/ft_strmapi.c
+++ function/ft_strmapi.c
@@ -1,29 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: eelkabia <eelkabia@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/23 15:00:44 by eelkabia          #+#    #+#             */
-/*   Updated: 2024/10/27 14:13:47 by eelkabia         ###   ########.fr       */
+/*   Created: 2024/10/27 14:30:31 by eelkabia          #+#    #+#             */
+/*   Updated: 2024/10/27 14:44:49 by eelkabia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	size_t	i;
+	char			*str;
+	unsigned int	i;
+	size_t			len;
 
+	if (!s || !f)
+		return (NULL);
 	i = 0;
-	if (!d || !c)
-		return (d);
-	while (i < n)
+	len = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	while (s[i])
 	{
-		(unsigned char *)dest[i] = (unsigned char *)src[i];
+		str[i] = f(i, s[i]);
 		i++;
 	}
-	(unsigned char *)dest[i] = '\0';
-	return ((unsigned char *)dest);
+	str[i] = '\0';
+	return (str);
 }
